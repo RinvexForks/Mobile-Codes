dbcecb440815ed70e627728730adf94fb72f2a52
Brian Faust <hello@brianfaust.de>
2016-11-04 06:35:38 +0200

    first

---
+++ src/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use BrianFaust\Payload\Json;
+use BrianFaust\Payload\YamlInline;
+use BrianFaust\Payload\Yaml;
+
+$crawler = (new Goutte\Client())->request('GET', 'http://mcc-mnc.com/');
+
+$datasets = [];
+$crawler->filter('table > tbody > tr')->each(function ($node) use (&$datasets) {
+    $datasets[] = [
+        'mcc' => $node->filter('td:nth-child(1)')->text(),
+        'mnc' => $node->filter('td:nth-child(2)')->text(),
+        'iso' => $node->filter('td:nth-child(3)')->text(),
+        'country_name' => $node->filter('td:nth-child(4)')->text(),
+        'country_code' => $node->filter('td:nth-child(5)')->text(),
+        'network' => $node->filter('td:nth-child(6)')->text(),
+    ];
+});
+
+$typeJson = new Json();
+$typeYaml = new Yaml();
+$typeYamlInline = new YamlInline();
+
+$typeJson->write(__DIR__.'/../dist/unsorted/data.json', $datasets);
+$typeYaml->write(__DIR__.'/../dist/unsorted/data.yml', $datasets);
+$typeYamlInline->write(__DIR__.'/../dist/unsorted/data-inline.yml', $datasets);
+
+$records = [];
+foreach ($datasets as $dataset) {
+    $records[$dataset['country_name']][] = $dataset;
+}
+
+$typeJson->write(__DIR__.'/../dist/sorted-by-country/data.json', $records);
+$typeYaml->write(__DIR__.'/../dist/sorted-by-country/data.yml', $records);
+$typeYamlInline->write(__DIR__.'/../dist/sorted-by-country/data-inline.yml', $records);
---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BrianFaust\Tests\MobileCodes;
+
+class ExampleTest extends AbstractTestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
